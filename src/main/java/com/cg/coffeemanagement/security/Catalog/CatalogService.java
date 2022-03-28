ac0ad7bf17696a07205e5ee6660c98b754fc9bff
NhatQuangCodeGym <lnquang0109@gmail.com>
2022-03-28 11:15:36 +0700

    add drinh

---
+++ src/main/java/com/cg/coffeemanagement/security/Catalog/CatalogService.java
@@ -0,0 +1,38 @@
+package com.cg.coffeemanagement.security.Catalog;
+
+import com.cg.coffeemanagement.model.Catalog;
+import com.cg.coffeemanagement.services.IGeneralServices;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CatalogService extends IGeneralServices<Catalog> {
+
+    @Override
+    List<Catalog> findAll();
+
+    @Override
+    Optional<Catalog> findById(Long id);
+
+    @Override
+    Catalog getById(Long id);
+
+    @Override
+    Catalog save(Catalog catalog);
+
+    @Override
+    void remove(Long id);
+
+    void deleteCatalog(@Param("id") Long id);
+
+    void restoreCatalog(@Param("id") Long id);
+
+    List<Catalog> findAllNotDeleted();
+
+    List<Catalog> findAllDeleted();
+
+    void deletedDrinkByCatalog(@Param("idCatalog") Long idCatalog);
+
+    void restoreDrinkByCatalog(@Param("idCatalog") Long idCatalog);
+}
---
+++ src/main/java/com/cg/coffeemanagement/security/Drink/DrinkServiceImpl.java
@@ -0,0 +1,72 @@
+package com.cg.coffeemanagement.security.Drink;
+
+
+import com.cg.coffeemanagement.model.Drink;
+import com.cg.coffeemanagement.repository.Drink.DrinkRepository;
+import com.cg.coffeemanagement.services.Drink.DrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class DrinkServiceImpl implements DrinkService {
+
+
+    @Autowired
+    DrinkRepository drinkRepository;
+
+    @Override
+    public void deleteDrinkById(Long id) {
+            drinkRepository.deleteDrinkById(id);
+    }
+
+    @Override
+    public void restoreDrink(Long id) {
+        drinkRepository.restoreDrink(id);
+
+    }
+
+    @Override
+    public List<Drink> findAllNotDeleted() {
+        return drinkRepository.findAllNotDeleted();
+    }
+
+    @Override
+    public List<Drink> findAllDeleted() {
+        return drinkRepository.findAllDeleted();
+    }
+
+    @Override
+    public Integer countDrink() {
+        return drinkRepository.countDrink();
+    }
+
+    @Override
+    public List<Drink> findAll() {
+        return drinkRepository.findAll();
+    }
+
+    @Override
+    public Optional<Drink> findById(Long id) {
+        return drinkRepository.findById(id);
+    }
+
+    @Override
+    public Drink getById(Long id) {
+        return drinkRepository.getById(id);
+    }
+
+    @Override
+    public Drink save(Drink drink) {
+        return drinkRepository.save(drink);
+    }
+
+    @Override
+    public void remove(Long id) {
+        drinkRepository.deleteById(id);
+    }
+}
