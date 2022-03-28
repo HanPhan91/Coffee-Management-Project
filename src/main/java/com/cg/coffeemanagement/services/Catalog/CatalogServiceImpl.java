4be7ceec3fe749781fc4d89d3d6c3ebe8e7d5a06
NhatQuangCodeGym <lnquang0109@gmail.com>
2022-03-28 10:24:20 +0700

    AddDrink

---
+++ src/main/java/com/cg/coffeemanagement/services/Catalog/CatalogServiceImpl.java
@@ -0,0 +1,72 @@
+package com.cg.coffeemanagement.services.Catalog;
+
+import com.cg.coffeemanagement.model.Catalog;
+import com.cg.coffeemanagement.repository.Catalog.CatalogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CatalogServiceImpl implements CatalogService{
+
+    @Autowired
+    CatalogRepository catalogRepository;
+
+    @Override
+    public List<Catalog> findAll() {
+        return catalogRepository.findAll();
+    }
+
+    @Override
+    public Optional<Catalog> findById(Long id) {
+        return catalogRepository.findById(id);
+    }
+
+    @Override
+    public Catalog getById(Long id) {
+        return catalogRepository.getById(id);
+    }
+
+    @Override
+    public Catalog save(Catalog catalog) {
+        return catalogRepository.save(catalog);
+    }
+
+    @Override
+    public void remove(Long id) {
+    }
+
+    @Override
+    public void deleteCatalog(Long id) {
+        catalogRepository.deleteCatalog(id);
+    }
+
+    @Override
+    public void restoreCatalog(Long id) {
+        catalogRepository.restoreCatalog(id);
+    }
+
+    @Override
+    public List<Catalog> findAllNotDeleted() {
+        return catalogRepository.findAllNotDeleted();
+    }
+
+    @Override
+    public List<Catalog> findAllDeleted() {
+        return catalogRepository.findAllDeleted();
+    }
+
+    @Override
+    public void deletedDrinkByCatalog(Long idCatalog) {
+            catalogRepository.deletedDrinkByCatalog(idCatalog);
+    }
+
+    @Override
+    public void restoreDrinkByCatalog(Long idCatalog) {
+            catalogRepository.restoreDrinkByCatalog(idCatalog);
+    }
+}
