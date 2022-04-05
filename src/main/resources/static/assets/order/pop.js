00433c7375b51c9047b9aaffc196b55e0d0546b8
NhatQuangCodeGym <lnquang0109@gmail.com>
2022-04-05 16:51:09 +0700

    <Chốt order>

--- src/main/resources/static/assets/order/pop.js
+++ src/main/resources/static/assets/order/pop.js
@@ -38,59 +38,25 @@ function getAllDrink() {
         })
 }
 
+class OrderItem {
+    constructor( quantity, totalPrice, drink,name){
 
-
-
-
-function Drink(id,name,quantity,price){
-    this.id = id;
-    this.name = name;
-    this.quantity = quantity;
-    this.price = price;
-}
-
-for (let i = 0; i < listDrink.length; i++) {
-    Drinks.push(new Drink(listDrink[i].id, listDrink[i].drinkName, 0, listDrink[i].price));
-}
-
-
-class Order {
-    constructor(id, quantity, totalPrice, drink, table){
-        this.id = id;
         this.quantity = quantity;
         this.totalPrice = totalPrice;
         this.drink = drink;
-        this.table = table;
+        this.name = name;
     }
 
-}
-function handlerButtonAddCart() {
-    $("button.addCart").on("click", function () {
-        let id = $(this).data("id");
-        addToCart(id);
-    });
-}
-
-function addToCart(id) {
-    let drink = Drinks.find(function (drink, index) {
-        return drink.id == id;
-    });
-    let check = checkCart(drink);
-    if (check == true) {
-        drink.drinkQuantity = 1;
-        Order.push(drink);
-        showCart(drink);
-        submitCart();
-    }
 }
 
 
 function addOrderItem() {
-let str;
+let str = "";
 let show =$(".product-cart-item");
     // let show = `<div className="product-cart-item" id="${OrderId}">`;
     show.empty();
     for (let i = 0; i < order.length; i++) {
+
 
         str += `
         <kv-cashier-cart-item class="row-list row-list-active">
@@ -125,7 +91,7 @@ let show =$(".product-cart-item");
                 <div class="cell-change-price">
                     <div class="popup-anchor">
                         <button
-                                class="form-control form-control-sm"> ${order[i].price}
+                                class="form-control form-control-sm"> ${order[i].totalPrice}
                         </button>
                     </div>
                 </div>
@@ -147,7 +113,8 @@ let show =$(".product-cart-item");
 function addOrderItemToOrder(orderId, drinkId) {
     for (let i = 0; i < listDrink.length; i++) {
         if (drinkId == listDrink[i].id) {
-            order.push(listDrink[i]);
+            let orderItem = new OrderItem(1,listDrink[i].price,drinkId,listDrink[i].name);
+            order.push(orderItem);
             addOrderItem();
         }
     }
@@ -196,105 +163,105 @@ function getAllTable() {
                         OrderId = item.id;
 
 
-                        $.ajax({
-                            type: "POST",
-                            url: "/api/orders/" + idtable,
-
-                        })
-                            .done(function (data) {
-
-                                OrderId = data.id;
-                                if (data.orderItem.length == 0) {
-                                    let emptyStr = `<div _ngcontent-aqe-c6="" class="page-empty"><i _ngcontent-aqe-c6="" class="mask mask-food"></i><div _ngcontent-aqe-c6="" class="empty-content" translate=""> Chưa có món nào <span _ngcontent-aqe-c6="" translate="">Vui lòng chọn món trong thực đơn</span></div></div>`
-                                    $("#listCartItem").html(emptyStr);
-
-                                } else {
-                                   //  //Vòng lặp Cart Item
-                                   //  data.forEach(function (item) {
-                                   //
-                                   //      listCartItem.append(`
-                                   //  <div class="cell-action"><a
-                                   //                          class="btn-icon btn-trash" href="javascript:void(0);"
-                                   //                          title="Xóa hàng hóa"><i
-                                   //                          class="far fa-trash-alt"></i></a></div>
-                                   //                  <div class="cell-order"> 1
-                                   //
-                                   //                      <div>
-                                   //                          <button class="btn-icon" type="button"><i
-                                   //                                  class="fa fa-star-o"></i></button>
-                                   //                      </div>
-                                   //                  </div>
-                                   //                  <div class="row-product">
-                                   //                      <div class="cell-name full" title="">
-                                   //                          <div class="wrap-name">
-                                   //                              <h4> ${item.name}
-                                   //
-                                   //
-                                   //                              </h4><span class="wrap-icons">
-                                   //
-                                   // </span>
-                                   //                          </div>
-                                   //
-                                   //                          <ul class="comboset-list-item">
-                                   //
-                                   //                          </ul>
-                                   //                          <div class="wrap-note" href="javascript:void(0)"><label
-                                   //                          >
-                                   //                              <button class="btn-icon has-Update">
-                                   //
-                                   //                                  <span class="note-hint">Ghi chú...</span>
-                                   //                                  <i class="fa fa-pencil"></i>
-                                   //                              </button>
-                                   //                          </label></div>
-                                   //                          <div class="list-topping">
-                                   //
-                                   //                          </div>
-                                   //                      </div>
-                                   //
-                                   //                      <div class="cell-quatity">
-                                   //                          <div class="cell-quantity-inner">
-                                   //                              <button class="btn-icon down" type="button"><i
-                                   //                                      class="fas fa-minus-circle"></i></button>
-                                   //
-                                   //                              <button
-                                   //                                      class="form-control form-control-sm item-quantity">
-                                   //                                  1
-                                   //                              </button>
-                                   //                              <button class="btn-icon up" type="button"><i
-                                   //                                      class="fas fa-plus-circle"></i></button>
-                                   //
-                                   //
-                                   //                          </div>
-                                   //                      </div>
-                                   //                      <div class="cell-warning">
-                                   //
-                                   //                      </div>
-                                   //
-                                   //                      <div class="cell-change-price">
-                                   //                          <div class="popup-anchor">
-                                   //                              <button
-                                   //                                      class="form-control form-control-sm"> ${item.price}
-                                   //                              </button>
-                                   //
-                                   //                          </div>
-                                   //                      </div>
-                                   //                      <div class="cell-price">
-                                   //                      <label for="cartItemPrice" class="form-label fw-bold">Tổng tiền</label>
-                                   //                      <input type="text" class="form-control" id="cartItemPrice" name="cartItemPrice">
-                                   //                       </div>
-                                   //                      <div class="cell-actions">
-                                   //                          <div class="btn-group" dropdown="">
-                                   //                              <button class="dropdown-toggle" type="button"
-                                   //                                      title="Thêm dòng"><i class="far fa-plus"></i>
-                                   //                              </button>
-                                   //                          </div>
-                                   //                      </div>
-                                   //                  </div>
-                                   //  `)
-                                   //  });
-                                    //Hết vòng lặp cart item
-                                }
-                            })
+                        // $.ajax({
+                        //     type: "POST",
+                        //     url: "/api/orders/" + idtable,
+                        //
+                        // })
+                        //     .done(function (data) {
+                        //
+                        //         OrderId = data.id;
+                        //         if (data.orderItem.length == 0) {
+                        //             let emptyStr = `<div _ngcontent-aqe-c6="" class="page-empty"><i _ngcontent-aqe-c6="" class="mask mask-food"></i><div _ngcontent-aqe-c6="" class="empty-content" translate=""> Chưa có món nào <span _ngcontent-aqe-c6="" translate="">Vui lòng chọn món trong thực đơn</span></div></div>`
+                        //             $("#listCartItem").html(emptyStr);
+                        //
+                        //         } else {
+                        //            //  //Vòng lặp Cart Item
+                        //            //  data.forEach(function (item) {
+                        //            //
+                        //            //      listCartItem.append(`
+                        //            //  <div class="cell-action"><a
+                        //            //                          class="btn-icon btn-trash" href="javascript:void(0);"
+                        //            //                          title="Xóa hàng hóa"><i
+                        //            //                          class="far fa-trash-alt"></i></a></div>
+                        //            //                  <div class="cell-order"> 1
+                        //            //
+                        //            //                      <div>
+                        //            //                          <button class="btn-icon" type="button"><i
+                        //            //                                  class="fa fa-star-o"></i></button>
+                        //            //                      </div>
+                        //            //                  </div>
+                        //            //                  <div class="row-product">
+                        //            //                      <div class="cell-name full" title="">
+                        //            //                          <div class="wrap-name">
+                        //            //                              <h4> ${item.name}
+                        //            //
+                        //            //
+                        //            //                              </h4><span class="wrap-icons">
+                        //            //
+                        //            // </span>
+                        //            //                          </div>
+                        //            //
+                        //            //                          <ul class="comboset-list-item">
+                        //            //
+                        //            //                          </ul>
+                        //            //                          <div class="wrap-note" href="javascript:void(0)"><label
+                        //            //                          >
+                        //            //                              <button class="btn-icon has-Update">
+                        //            //
+                        //            //                                  <span class="note-hint">Ghi chú...</span>
+                        //            //                                  <i class="fa fa-pencil"></i>
+                        //            //                              </button>
+                        //            //                          </label></div>
+                        //            //                          <div class="list-topping">
+                        //            //
+                        //            //                          </div>
+                        //            //                      </div>
+                        //            //
+                        //            //                      <div class="cell-quatity">
+                        //            //                          <div class="cell-quantity-inner">
+                        //            //                              <button class="btn-icon down" type="button"><i
+                        //            //                                      class="fas fa-minus-circle"></i></button>
+                        //            //
+                        //            //                              <button
+                        //            //                                      class="form-control form-control-sm item-quantity">
+                        //            //                                  1
+                        //            //                              </button>
+                        //            //                              <button class="btn-icon up" type="button"><i
+                        //            //                                      class="fas fa-plus-circle"></i></button>
+                        //            //
+                        //            //
+                        //            //                          </div>
+                        //            //                      </div>
+                        //            //                      <div class="cell-warning">
+                        //            //
+                        //            //                      </div>
+                        //            //
+                        //            //                      <div class="cell-change-price">
+                        //            //                          <div class="popup-anchor">
+                        //            //                              <button
+                        //            //                                      class="form-control form-control-sm"> ${item.price}
+                        //            //                              </button>
+                        //            //
+                        //            //                          </div>
+                        //            //                      </div>
+                        //            //                      <div class="cell-price">
+                        //            //                      <label for="cartItemPrice" class="form-label fw-bold">Tổng tiền</label>
+                        //            //                      <input type="text" class="form-control" id="cartItemPrice" name="cartItemPrice">
+                        //            //                       </div>
+                        //            //                      <div class="cell-actions">
+                        //            //                          <div class="btn-group" dropdown="">
+                        //            //                              <button class="dropdown-toggle" type="button"
+                        //            //                                      title="Thêm dòng"><i class="far fa-plus"></i>
+                        //            //                              </button>
+                        //            //                          </div>
+                        //            //                      </div>
+                        //            //                  </div>
+                        //            //  `)
+                        //            //  });
+                        //             //Hết vòng lặp cart item
+                        //         }
+                        //     })
 
                         document.getElementById("showCart").style.display = "block";
 
@@ -329,6 +296,24 @@ function getAllTable() {
         getAllDrink();
         document.getElementById("showCart").style.display = "none";
 
+    });
+
+    $("#createOrder").on('click', function () {
+       $.ajax({
+           headers: {
+               "Accept": "application/json",
+               "Content-type": "application/json"
+           },
+           type: "POST",
+           url: "/api/orders/create/"+ OrderId + "?discount=",
+           data: JSON.stringify(order)
+       })
+           .done(function (data) {
+               swal("thành công", "tạo order thành công","success");
+           })
+           .fail(function (resp){
+               console.log(resp);
+           })
     });
 
 
