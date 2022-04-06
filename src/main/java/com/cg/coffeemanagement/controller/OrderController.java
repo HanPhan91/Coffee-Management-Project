834094cd40f2fc486503d7b5b0815247e67f9939
NhatQuangCodeGym <lnquang0109@gmail.com>
2022-04-06 11:43:06 +0700

    <sự kiện table>

--- src/main/java/com/cg/coffeemanagement/controller/OrderController.java
+++ src/main/java/com/cg/coffeemanagement/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.cg.coffeemanagement.controller;
 import com.cg.coffeemanagement.model.CoffeeTable;
+import com.cg.coffeemanagement.model.Discount;
 import com.cg.coffeemanagement.model.Drink;
 import com.cg.coffeemanagement.services.Order.OrderService;
 import com.cg.coffeemanagement.services.CoffeeTable.CoffeeTableService;
 import com.cg.coffeemanagement.services.Drink.DrinkService;
+import com.cg.coffeemanagement.services.discount.IDiscountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,7 @@ public class OrderController {
 
     @Autowired
     DrinkService drinkService;
+
 
     @GetMapping
     public ModelAndView listOrder(){
